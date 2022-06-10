4d2d6626a743dd36917578d01ceae2ec5d9fd6ed
Michał Jakóbczyk <michal.jakobczyk1@wp.pl>
2022-06-10 22:48:20 +0200

    missing sattelite exception fix

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Baloons
+namespace WPF_Balloon_Popping_Game_Moo_ICT
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,9 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+
 using System.Windows.Threading;
 
-namespace Baloons
+namespace WPF_Balloon_Popping_Game_Moo_ICT
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -28,15 +29,21 @@ namespace Baloons
         Random rand = new Random();
 
         List<Rectangle> itemRemover = new List<Rectangle>();
+
         ImageBrush backgroundImage = new ImageBrush();
 
         int balloonSkins;
         int i;
+
         int missedBalloons;
+
         bool gameIsActive;
+
         int score;
 
         MediaPlayer player = new MediaPlayer();
+
+
         public MainWindow()
         {
             InitializeComponent();
@@ -44,14 +51,16 @@ namespace Baloons
             gameTimer.Tick += GameEngine;
             gameTimer.Interval = TimeSpan.FromMilliseconds(20);
 
-            backgroundImage.ImageSource = new BitmapImage(new Uri("/images/background.png"));
+            backgroundImage.ImageSource = new BitmapImage(new Uri("pack://application:,,,/files/background-Image.jpg"));
             MyCanvas.Background = backgroundImage;
 
             RestartGame();
         }
+
         private void GameEngine(object sender, EventArgs e)
         {
-            ScoreText.Content = "Score: " + score;
+            scoreText.Content = "Score: " + score;
+
             intervals -= 10;
 
             if (intervals < 1)
@@ -67,27 +76,27 @@ namespace Baloons
 
                 switch (balloonSkins)
                 {
-                    case 1:
-                        balloonImage.ImageSource = new BitmapImage(new Uri("/image/balloon1.png"));
+                    case 1: 
+                       balloonImage.ImageSource = new BitmapImage(new Uri("pack://application:,,,/files/balloon1.png"));
                         break;
                     case 2:
-                        balloonImage.ImageSource = new BitmapImage(new Uri("/image/balloon2.png"));
+                        balloonImage.ImageSource = new BitmapImage(new Uri("pack://application:,,,/files/balloon2.png"));
                         break;
                     case 3:
-                        balloonImage.ImageSource = new BitmapImage(new Uri("/image/balloon3.png"));
+                        balloonImage.ImageSource = new BitmapImage(new Uri("pack://application:,,,/files/balloon3.png"));
                         break;
                     case 4:
-                        balloonImage.ImageSource = new BitmapImage(new Uri("/image/balloon4.png"));
+                        balloonImage.ImageSource = new BitmapImage(new Uri("pack://application:,,,/files/balloon4.png"));
                         break;
                     case 5:
-                        balloonImage.ImageSource = new BitmapImage(new Uri("/image/balloon5.png"));
+                        balloonImage.ImageSource = new BitmapImage(new Uri("pack://application:,,,/files/balloon5.png"));
                         break;
                 }
 
                 Rectangle newBalloon = new Rectangle
                 {
                     Tag = "balloon",
-                    Height = 50,
+                    Height = 70,
                     Width = 50,
                     Fill = balloonImage
                 };
@@ -102,40 +111,73 @@ namespace Baloons
 
             foreach (var x in MyCanvas.Children.OfType<Rectangle>())
             {
+
                 if ((string)x.Tag == "balloon")
                 {
+
                     i = rand.Next(-5, 5);
+
                     Canvas.SetTop(x, Canvas.GetTop(x) - speed);
                     Canvas.SetLeft(x, Canvas.GetLeft(x) - (i * -1));
+
                 }
 
                 if (Canvas.GetTop(x) < 20)
                 {
                     itemRemover.Add(x);
+
                     missedBalloons += 1;
                 }
+
+
             }
+
 
             foreach (Rectangle y in itemRemover)
             {
                 MyCanvas.Children.Remove(y);
             }
+
+
+
+            if (missedBalloons > 10)
+            {
+                gameIsActive = false;
+                gameTimer.Stop();
+                MessageBox.Show("Game over!! You missed 10 Balloons" + Environment.NewLine + "Click ok to play again");
+
+                RestartGame();
+            }
+
+            if (score > 3)
+            {
+                speed = 7;
+            }
+
+
+
         }
 
         private void PopBalloons(object sender, MouseButtonEventArgs e)
         {
             if (gameIsActive)
             {
+
                 if (e.OriginalSource is Rectangle)
                 {
+
                     Rectangle activeRec = (Rectangle)e.OriginalSource;
+
+                    player.Open(new Uri("../../files/pop_sound.mp3", UriKind.RelativeOrAbsolute));
                     player.Play();
 
                     MyCanvas.Children.Remove(activeRec);
+
                     score += 1;
                 }
             }
         }
+
         private void StartGame()
         {
             gameTimer.Start();
@@ -146,6 +188,7 @@ namespace Baloons
             gameIsActive = true;
             speed = 3;
         }
+
         private void RestartGame()
         {
             foreach (var x in MyCanvas.Children.OfType<Rectangle>())
@@ -159,6 +202,12 @@ namespace Baloons
             }
 
             itemRemover.Clear();
+
+            StartGame();
+
+
+
         }
+
     }
 }
